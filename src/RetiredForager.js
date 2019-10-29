0f4c4c97215dd517952e825f33745d8e1e9d40b4
Eui Hyun Kim <hyomithetech@gmail.com>
2019-10-28 19:51:56 -0700

    Finish the beesbeesbees assignment

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = "gamble";
+  this.color = "grey";
+  this.canFly = false;
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead";
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
